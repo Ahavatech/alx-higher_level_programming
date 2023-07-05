6b58f54aa6818690f4bb565b4c69c02c0187eaea
Ahavatech <ahavatechng@gmail.com>
2023-07-05 13:00:27 +0100

    zinger

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Defines an integer addition function
-
-:param a: int/float
-:param b: int/float
+"""
+Module 0-add_integer
+Contains one method that returns an int sum
+Accepts two values, whether int or float, and casts them to int before adding
 """
 
 
 def add_integer(a, b=98):
-    """Adds tow integers/floats together
-
-    :return: int"""
-
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    """ Adds 2 integers
+    Args: a, b
+    Return a + b"""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+
+    return int(a) + int(b)
