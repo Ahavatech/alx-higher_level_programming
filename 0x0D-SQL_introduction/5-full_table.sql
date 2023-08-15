0976a4bd1642e0cf440a721e542f310ed3df8142
Ahavatech <ahavatechng@gmail.com>
2023-08-15 23:00:46 +0100

    0-9

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,52 @@
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+5-full_table.sql [unix] (00:59 01/01/1970)                                                                                                                                             0,0-1 All
+"5-full_table.sql" [New]                                                                                                                                                             9
+-- script that prints -- script that prints the full description of the table first_table
+SHOW CREATE TABLE first_tablethe full description of the table first_table
+SHOW CREATE TABLE first_table
