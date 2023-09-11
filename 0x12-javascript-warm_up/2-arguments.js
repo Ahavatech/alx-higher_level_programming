a5bbe940d575a6e251fefd2277385ddcf4cc6964
[Ibukunoluwa Adekunle] <[ibkadekunle2004@gmail.com]>
2023-09-11 17:59:44 +0100

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+// Check the number of arguments
+if (process.argv.length === 2) {
+  // No arguments passed
+  console.log("No argument");
+} else if (process.argv.length === 3) {
+  // One argument passed
+  console.log("Argument found");
+} else {
+  // More than one argument passed
+  console.log("Arguments found");
+}
+
